1b6a83ae492bf4993d09ad1bd05b3cab32d4bcb9
Rufai AbdulWasii <rufaiwasiu5@gmail.com>
2022-06-25 23:52:39 +0100

    django app

---
+++ I4G003785C5D/blog/views.py
@@ -0,0 +1,27 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .models import Post 
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = " __all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class DeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# django-crud
+# special-spork
